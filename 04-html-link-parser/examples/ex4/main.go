30b93997f85f9b1322f31ebe95f85d32b1b17b8d
sharad-pizza-hut <sharad.shekar@yum.com>
2020-02-20 00:21:18 -0600

    04 - Parsing examples

---
+++ 04-html-link-parser/examples/ex4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	link "gophercises/04-html-link-parser"
+	"strings"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
